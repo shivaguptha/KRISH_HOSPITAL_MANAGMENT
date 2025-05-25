15105f2ce224ec2fbc2779423984f323448ba173
shivaguptha <152949895+shivaguptha@users.noreply.github.com>
2025-05-25 18:26:03 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     try {
       await axios
         .post(
-          "http://localhost:4000/api/v1/user/login",
+          "https://h-krish-backend.onrender.com/api/v1/user/login",
           { email, password, role: "Patient" },
           {
             withCredentials: true,
